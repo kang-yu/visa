63d4d95237f830004f16c727f2538d0a28726d4b
kang-yu <kang.yu@live.com>
2019-03-16 23:27:04 +0100

    remove the ZZZ-

--- ZZZ-Rx/SpecFun.R
+++
@@ -1,4 +0,0 @@
-#'
-#' To define a group of SpecFun
-#' (https://rdrr.io/r/methods/Methods_for_Nongenerics.html)
-#'
--- ZZZ-Rx/Spectral-method.R
+++
@@ -1,6 +0,0 @@
-#' @include visa.R
-#'
-#' To be further developed ...
-#'
-#' @export
-setMethod("length", "SpectralDataFrame", function(x) length(x@wavelength))
