f7262cd37792ca49c8fb5105bfa2faa82c26e3a1
Kang Yu <kang.yu@outlook.com>
2021-04-16 18:22:01 +0200

    remove the line <<<<<<<< HEAD due to merge

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 library(testthat)
 library(visa)
 
 test_check("visa")
-=======
-library(testthat)
-library(visa)
-
-test_check("visa")
->>>>>>> fdcc887ca45316c73fbe6ef70768b77deffbc575
--- tests/testthat/test-ggplot.lmfit.R
+++ tests/testthat/test-ggplot.lmfit.R
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 test_that("Test my ggplot.lmfit function: ",
           {
             expect_true(is.ggplot(ggplot.lmfit(1:10, 2:11)))
           })
-=======
-test_that("Test my ggplot.lmfit function: ",
-          {
-            expect_true(is.ggplot(ggplot.lmfit(1:10, 2:11)))
-          })
->>>>>>> fdcc887ca45316c73fbe6ef70768b77deffbc575
