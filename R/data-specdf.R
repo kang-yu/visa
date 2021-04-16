ae1e4c34dd8eb1b7bcb987f5be9760b009bc181a
Kang Yu <kang.yu@outlook.com>
2021-04-16 18:17:21 +0200

    remove the line <<<<<<<< HEAD due to merge

--- R/data-specdf.R
+++ R/data-specdf.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #' Example data in the SpectraDataFrame format
 #'
 #' A dataset containing the plant Nitrogen content and spectra.
@@ -19,25 +18,4 @@
 #' @seealso \link{data.frame} and \code{\link{NSpec.DB}}
 #' @source \url{visa}
 "NSpec.DF"
-=======
-#' Example data in the SpectraDataFrame format
-#'
-#' A dataset containing the plant Nitrogen content and spectra.
-#' The Spectra matrix is stored as a variable (in a column) of a data.frame.
-#'
-#' @aliases Data-SpectraDataFrame
-#' @format A data frame with 19 rows and 2 variables:
-#'
-#' \describe{
-#'   \item{N}{Plant nitrogen content}
-#'   \item{spectra}{A variable of Matrix of plant spectra}
-#'   ...
-#' }
-#' @examples
-#' library(visa)
-#' data(NSpec.DF)
-#' str(NSpec.DF)
-#' @seealso \link{data.frame} and \code{\link{NSpec.DB}}
-#' @source \url{visa}
-"NSpec.DF"
->>>>>>> fdcc887ca45316c73fbe6ef70768b77deffbc575
+
