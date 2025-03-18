1b5bdf1e3c12d39d63aedee66e18bdf358cfe89a
Kang Yu <kang.yu@live.com>
2025-03-19 00:02:56 +0100

    testthat test-ggplot.lmfit added conditional skip

--- tests/testthat/test-ggplot.lmfit.R
+++ tests/testthat/test-ggplot.lmfit.R
@@ -1,3 +1,8 @@
+
+if (!capabilities("tcltk")) {
+  skip("Tcl/Tk not available; skipping all Tk-dependent tests in this file")
+}
+
 test_that("Test my ggplot.lmfit function: ",
           {
             expect_true(is.ggplot(ggplot.lmfit(1:10, 2:11)))
